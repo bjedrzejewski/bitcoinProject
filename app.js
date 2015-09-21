eebad775502af77fcb588e50a075325916fdaa11
bjedrzejewski <bjedrzejewski@scottlogic.com>
2015-09-21 14:39:24 +0100

    Small refactor of app.js.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-});
-server.listen(8888);
+
+function onRequest(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+}
+
+http.createServer(onRequest).listen(8888);
 
 /*
 var express = require('express');
